5b87b2c2a4a9bcedd4c31c38166d63202240f455
Danit <danit120@gmail.com>
2023-12-28 16:40:36 +0200

    WORKING - NO AWS

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-
 const { Telegraf } = require('telegraf');
 const { setupBot } = require('./Bot');
-const botToken = '6672454870:AAH1-OEYPYSwmBVeLJ0pxxe8PVgwG8385BU'; // Use environment variable for security
+
+const botToken = '6672454870:AAH1-OEYPYSwmBVeLJ0pxxe8PVgwG8385BU';
+// Create a new instance of Telegraf using the bot token
 const bot = new Telegraf(botToken);
 
-// Set up the bot with commands and listeners
+// Set up the bot by calling the setupBot function
 setupBot(bot);
 
-exports.handler = async (event, context) => {
-  try {
-    console.log('Received event:', event);
+// Launch the bot
+bot.launch();
 
-    if (!event.body) {
-      console.error('No body found in the event');
-      return { statusCode: 400, body: 'Bad Request: No body found' };
-    }
 
-    let update;
-    try {
-      update = JSON.parse(event.body);
-	  console.log('Parsed update:', update);
-
-    } catch (error) {
-      console.error('Error parsing event body:', error);
-      return { statusCode: 400, body: 'Bad Request: Invalid JSON' };
-    }
-
-    await bot.handleUpdate(update);
-    return { statusCode: 200, body: '' };
-  } catch (error) {
-    console.error('Error in Lambda function:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Internal Server Error',
-        error: error.message
-      }),
-    };
-  }
-};
